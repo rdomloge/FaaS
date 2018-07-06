5fdf1d5d213f7859907a41226c631bd545383d9b
Ramsay Domloge <ramsay.domloge@bca.com>
2018-07-06 13:48:42 +0100

    Initial hack of main classes

--- src/main/java/com/example/faas/reactor/rmq/Receiver.java
+++ src/main/java/com/example/faas/reactor/rmq/Receiver.java
@@ -3,17 +3,20 @@ package com.example.faas.reactor.rmq;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.example.faas.common.JobRequest;
 import com.example.faas.ex.FunctionException;
 import com.example.faas.reactor.FunctionOrchestration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Component
 public class Receiver {
 	
 	@Autowired
 	private ObjectMapper mapper;
 	
+	@Autowired
 	private FunctionOrchestration orchestration;
 
 	public void receive(String msg) throws IOException, FunctionException {
