0f1f223178a0b77b30356d244e059415a276aac0
Ramsay Domloge <ramsay.domloge@bca.com>
2018-10-22 20:41:30 +0100

    Switching to external Postgress DB

--- src/main/java/com/example/faas/sqlite/config/DbConfig.java
+++ src/main/java/com/example/faas/sqlite/config/DbConfig.java
@@ -12,8 +12,12 @@ public class DbConfig {
 	@Bean
 	public DataSource dataSource() {
 	        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-	        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-	        dataSourceBuilder.url("jdbc:sqlite:faas.db");
+//	        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+//	        dataSourceBuilder.url("jdbc:sqlite:faas.db");
+	        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+	        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/faas");
+	        dataSourceBuilder.password("password");
+	        dataSourceBuilder.username("faas");
 	        return dataSourceBuilder.build();   
 	}
 
